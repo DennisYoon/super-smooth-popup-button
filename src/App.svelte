bcac269574133ff00d2b4b74015568d4330f7869
DennisYun <2008dennisyun@gmail.com>
2023-02-27 17:17:49 +0900

    generating pop up button component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+  import Button from "./components/Button.svelte";
 </script>
 
 <main>
+  <h1>a Journey to the Journal</h1>
+  <Button content="hello world" width="50" height="50">
+    hello guys
+  </Button>
 </main>
 
-<style>
+<style lang="scss">
+  h1 {
+    text-align: center;
+  }
 </style>

7c102b0a10772220c8450dc499bbcc57d22f2dc5
DennisYun <2008dennisyun@gmail.com>
2023-03-11 17:03:08 +0900

    팝업 버튼들을 JournalList 컴포넌트로 변환

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
-  import PopupButton from "./components/PopupButton/PopupButton.svelte";
+  import JournalList from "./components/JournalList/JournalList.svelte";
 </script>
 
 <main>
   <!-- <Background /> -->
   <h1>a Journey to the Journal</h1>
-  <div id="date">
-    {#each Array(1).fill(1).map((v, i) => v + i) as i}
-      <PopupButton
-        originalWidth="auto"
-        originalHeight="50px"
-        popupWidth="400px"
-        popupHeight="500px"
-        className="hello"
-      >
-        <div slot="before">
-          {i}
-        </div>
-        <div slot="after">
-          <h2>
-            DO You Hear the people sing?
-          </h2>
-          <button>hello world</button>
-        </div>
-      </PopupButton>
-    {/each}
-  </div>
+  <JournalList></JournalList>
 </main>
 
 <style lang="scss">
@@ -43,17 +23,5 @@
     h1 {
       text-align: center;
     }
-    #date {
-      border: 1px solid black;
-    }
   }
-
-  :global(.hello) {
-    margin: 10px;
-    display: block !important;
-  }
-
-  
-
-  
 </style>
---
+++ src/components/JournalList/JournalList.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import PopupButton from "../PopupButton/PopupButton.svelte";
+</script>
+
+<main>
+  <div id="date">
+    {#each Array(1).fill(1).map((v, i) => v + i) as i}
+      <PopupButton
+        originalWidth="auto"
+        originalHeight="50px"
+        popupWidth="400px"
+        popupHeight="500px"
+        className="JournalList"
+      >
+        <div slot="before">
+          {i}
+        </div>
+        <div slot="after">
+          <h2>
+            DO You Hear the people sing?
+          </h2>
+          <button>hello world</button>
+        </div>
+      </PopupButton>
+    {/each}
+  </div>
+</main>
+
+<style lang="scss">
+  main {
+    #date {
+      border: 1px solid black;
+    }
+  }
+  
+  :global(.JournalList) {
+    margin: 10px;
+    display: block !important;
+  }
+</style>
